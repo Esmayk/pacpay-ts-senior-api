8d0f9e2dabe5e3434668694bdba281befcb6c05a
Esmayk Tillesse <esmaykalves@gmail.com>
2024-03-27 17:46:31 -0300

    commit init

---
+++ src/main/java/com/pacpay/PacpayTsSeniorApiApplication.java
@@ -0,0 +1,13 @@
+package com.pacpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacpayTsSeniorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacpayTsSeniorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pacpay/PacpayTsSeniorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacpayTsSeniorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
